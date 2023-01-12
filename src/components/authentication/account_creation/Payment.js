c6c61058b1e752b348be1f40db70c23b773f489a
chira37 <chirancjr@gmail.com>
2023-01-12 07:57:32 +0530

    Add stripe configuration

--- src/components/authentication/account_creation/Payment.js
+++ src/components/authentication/account_creation/Payment.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Checkout from "./components/Checkout";
 import Plans from "./components/Plans";
+// const stripePromise = loadStripe(stripePublishableKey);
 
 const Payment = () => {
+  const [clientSecrete, setClientSecrete] = useState(null);
   return (
     <div>
       <Plans />
-      {/* <Checkout /> */}
+      {/* <Elements stripe={stripePromise} options={{ clientSecret: clientSecrete }}>
+        <Checkout />
+      </Elements> */}
     </div>
   );
 };
--- src/components/authentication/account_creation/components/Checkout/Checkout.jsx
+++ src/components/authentication/account_creation/components/Checkout/Checkout.jsx
@@ -1,8 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
+import { useStripe, useElements, PaymentElement } from "@stripe/react-stripe-js";
+
 import "rc-slider/assets/index.css";
 import styles from "./checkout.module.scss";
 const Plans = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const result = await stripe.confirmPayment({
+      elements,
+      confirmParams: {},
+    });
+
+    if (result.error) {
+      console.log(result.error.message);
+    } else {
+    }
+  };
+
   return (
     <div>
       <h4 className="h2 mb-4">Checkout</h4>
@@ -14,6 +36,13 @@ const Plans = () => {
               members, allowing you to get to silence faster Access to Liam Boehm Video content available no where else
               on the internet, to get the best and most exclusive steps to silence
             </span>
+          </div>
+
+          <div>
+            <form onSubmit={handleSubmit}>
+              <PaymentElement />
+              <Button disabled={!stripe}>Submit</Button>
+            </form>
           </div>
         </Card.Body>
       </Card>
