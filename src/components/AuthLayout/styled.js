20652fcab986d43ca7f7b5b54a6a113154e9a03a
chira37 <chirancjr@gmail.com>
2023-02-28 20:02:48 +0530

    change the color

--- src/components/AuthLayout/styled.js
+++ src/components/AuthLayout/styled.js
@@ -1,7 +1,6 @@
 import { Box, styled } from "@mui/material";
 
 export const Wrapper = styled(Box)`
-  background-color: ${(props) => props.theme.palette.primary.main};
   min-height: 100vh;
   display: flex;
   align-items: center;
